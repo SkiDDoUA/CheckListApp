888c177f42bb31c8640b945a3d2e1d185893926a
Anton <antonlena516@gmail.com>
2022-11-23 12:40:53 +0200

    Initial Commit

---
+++ CheckListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckListApp
+//
+//  Created by Anton Kolesnikov on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
